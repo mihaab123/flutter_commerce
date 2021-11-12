ef19adce6ff6e9707ee61bd06cb03f973a4a053b
Defender <miha.ab.2017@gmail.com>
2021-11-12 21:39:57 +0200

    fix

--- lib/provider/product.dart
+++ lib/provider/product.dart
@@ -1,42 +1,43 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_commerce/db/product.dart';
 import 'package:flutter_commerce/models/product.dart';
 
-class ProductProvider with ChangeNotifier{
+class ProductProvider with ChangeNotifier {
   final ProductServices _productServices = ProductServices();
   List<ProductModel> products = [];
   List<ProductModel> productsSearched = [];
   List<ProductModel> productsFeatured = [];
-  Map<String,List<String>> productProperties = [] as Map<String, List<String>>;
-  List<String> productPropertiesList =[];
+  Map<String, List<String>> productProperties = [] as Map<String, List<String>>;
+  List<String> productPropertiesList = [];
 
-
-  ProductProvider.initialize(){
+  ProductProvider.initialize() {
     loadProducts();
     loadFeaturedProducts();
   }
 
-  Future<void> loadProducts()async{
+  Future<void> loadProducts() async {
     products = await _productServices.getProducts();
     notifyListeners();
   }
 
-  Future search({String productName})async{
-    productsSearched = await _productServices.searchProducts(productName: productName);
+  Future search({String productName}) async {
+    productsSearched =
+        await _productServices.searchProducts(productName: productName);
     notifyListeners();
   }
-  Future loadFeaturedProducts({String productName})async{
+
+  Future loadFeaturedProducts({String productName}) async {
     productsFeatured = await _productServices.getFeaturedProducts();
     notifyListeners();
   }
-  Future<void> loadProductProperties()async{
+
+  Future<void> loadProductProperties() async {
     productProperties.clear();
     productPropertiesList.clear();
-    for (final product in products){
+    for (final product in products) {
       // sizes
       List<String> tempList = [];
-      if(productProperties.containsKey("sizes")){
+      if (productProperties.containsKey("sizes")) {
         tempList = productProperties["sizes"];
       }
       for (final String size in product.sizes) {
@@ -49,25 +50,24 @@ class ProductProvider with ChangeNotifier{
 
       // colors
       tempList = [];
-      if(productProperties.containsKey("colors")){
+      if (productProperties.containsKey("colors")) {
         tempList = productProperties["colors"];
       }
       for (final color in product.colors) {
         if (!tempList.contains(color)) {
-          tempList.add(color);
+          tempList.add(color.toString());
         }
       }
       tempList.sort();
       productProperties["colors"] = tempList;
     }
     // properties list
-    if(productProperties.containsKey("sizes")) {
+    if (productProperties.containsKey("sizes")) {
       productPropertiesList.add("sizes");
     }
-    if(productProperties.containsKey("colors")) {
+    if (productProperties.containsKey("colors")) {
       productPropertiesList.add("colors");
     }
     notifyListeners();
   }
-
 }
